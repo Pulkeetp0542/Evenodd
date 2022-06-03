3757503754e3b03fef4377319a134a804d63cedf
Pulkeet.p0542 <pulkeet.p@outlook.com>
2022-06-03 08:28:14 +0530

    Statement changed in the Print section

--- Evenodd.py
+++ Evenodd.py
@@ -1,11 +1,11 @@
-a=int(input("Enter"))
+a=int(input("Enter the Number"))
 if a>1:
     for x in range(2,a):
         if(a%x==0):
-            print("Not Prime")
+            print("Number is Not Prime")
             break
         else:
-            print("Prime")
+            print("Number is Prime")
             break
 else:
-    print("Prime")
+    print("Number is Prime")
